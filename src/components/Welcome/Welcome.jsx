de3a2c9dd06d37a062a2955b76f8571b1a1bcb70
gustavo-atanazio <gus.atanazio@gmail.com>
2023-06-28 17:41:55 -0300

    Consumindo contexto

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,18 +1,24 @@
 import './Welcome.css';
 import questions from '../../data/questions';
+import { useContext } from 'react';
+import { QuizContext } from '../../context/quiz';
 
 const Welcome = () => {
-  return (
-    <div>
-        <h2>Bem vindo!</h2>
-        <p>Escolha um dos temas abaixo para começar</p>
-        <div className="buttons">
-            {questions.map(question => (
-                <button key={question.category}>{question.category}</button>
-            ))}
+    const [quizState, dispatch] = useContext(QuizContext);
+
+    return (
+        <div>
+            <h2>Bem vindo!</h2>
+            <p>Escolha um dos temas abaixo para começar</p>
+            <div className="buttons">
+                {questions.map(question => (
+                    <button key={question.category} onClick={() => dispatch({type: "CHANGE_STATE"})}>
+                        {question.category}
+                    </button>
+                ))}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Welcome;
