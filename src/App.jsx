f8b6bbb947ea3fb75a101f2f934ea903b872007c
gustavo-atanazio <gus.atanazio@gmail.com>
2023-06-28 17:42:14 -0300

    Consumindo contexto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
+import { useContext } from 'react';
+import { QuizContext } from './context/quiz';
 import Welcome from './components/Welcome/Welcome';
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
   return (
     <div className='App'>
       <h1>Quizzer</h1>
-      <Welcome/>
+      {quizState.gameStage === "Start" && <Welcome/>}
     </div>
   )
 }
