36d5c64474176651c0932a558f5f4dacf33cc850
gustavo-atanazio <gus.atanazio@gmail.com>
2023-06-29 14:58:08 -0300

    Recomeçando jogo e score

--- src/components/GameOver/GameOver.jsx
+++ src/components/GameOver/GameOver.jsx
@@ -1,9 +1,17 @@
 import './GameOver.css';
+import { useContext } from 'react';
+import { QuizContext } from '../../context/quiz';
 
 const GameOver = () => {
-  return (
-    <div>GameOver</div>
-  )
+    const [quizState, dispatch] = useContext(QuizContext);
+
+    return (
+        <div className='game-over'>
+            <h2>Fim de jogo!</h2>
+            <p>Você acertou {quizState.score} de {quizState.questions.length} perguntas</p>
+            <button onClick={() => dispatch({type: "NEW_GAME"})}>Jogar de novo</button>
+        </div>
+    )
 }
 
 export default GameOver;
