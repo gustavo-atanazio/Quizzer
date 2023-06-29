4b0d2757c09ebffbc2c413e2744ec56f3a1c3b95
gustavo-atanazio <gus.atanazio@gmail.com>
2023-06-29 12:18:53 -0300

    Repassando categoria e reordenando questões

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -6,13 +6,19 @@ import { QuizContext } from '../../context/quiz';
 const Welcome = () => {
     const [quizState, dispatch] = useContext(QuizContext);
 
+    function chooseCategoryAndReorderQuestions(category) {
+        dispatch({type: "START_GAME", payload: category})
+
+        dispatch({type: "REORDER_QUESTIONS"})
+    }
+
     return (
         <div>
             <h2>Bem vindo!</h2>
             <p>Escolha um dos temas abaixo para começar</p>
             <div className="buttons">
                 {data.map(question => (
-                    <button key={question.category} onClick={() => dispatch({type: "CHANGE_STATE"})}>
+                    <button key={question.category} onClick={() => chooseCategoryAndReorderQuestions(question.category)}>
                         {question.category}
                     </button>
                 ))}
