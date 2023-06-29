c31cd8c3188fbf58782f7b14519cdec03214ef4c
gustavo-atanazio <gus.atanazio@gmail.com>
2023-06-29 17:33:21 -0300

    Adicionando imagem de fim de jogo

--- src/components/GameOver/GameOver.jsx
+++ src/components/GameOver/GameOver.jsx
@@ -1,4 +1,5 @@
 import './GameOver.css';
+import endgame from '../../img/endgame.svg';
 import { useContext } from 'react';
 import { QuizContext } from '../../context/quiz';
 
@@ -9,6 +10,7 @@ const GameOver = () => {
         <div className='game-over'>
             <h2>Fim de jogo!</h2>
             <p>Você acertou {quizState.score} de {quizState.questions.length} perguntas</p>
+            <img src={endgame} alt="Fim de jogo"/>
             <button onClick={() => dispatch({type: "NEW_GAME"})}>Jogar de novo</button>
         </div>
     )
