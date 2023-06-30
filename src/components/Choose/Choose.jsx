573545c08c47335c68ae4deb95162aa61fe434d8
gustavo-atanazio <gus.atanazio@gmail.com>
2023-06-30 13:13:56 -0300

    Ajustando componente

--- src/components/Choose/Choose.jsx
+++ src/components/Choose/Choose.jsx
@@ -1,9 +1,29 @@
 import './Choose.css';
 
+import { useContext } from 'react';
+import { QuizContext } from '../../context/quiz';
+
 const Choose = () => {
-  return (
-    <div>Choose</div>
-  )
+    const [quizState, dispatch] = useContext(QuizContext);
+
+    function chooseCategoryAndReorderQuestions(category) {
+        dispatch({type: "START_GAME", payload: category})
+
+        dispatch({type: "REORDER_QUESTIONS"})
+    }
+
+    return (
+        <div>
+            <p>Escolha um dos temas abaixo</p>
+            <div className="buttons">
+                {quizState.questions.map(question => (
+                    <button key={question.category} onClick={() => chooseCategoryAndReorderQuestions(question.category)}>
+                        {question.category}
+                    </button>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default Choose;
