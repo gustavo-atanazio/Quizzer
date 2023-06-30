bec4c24a62d65da12554577b0c7b6cb380960a4d
gustavo-atanazio <gus.atanazio@gmail.com>
2023-06-30 12:43:46 -0300

    Componente Choose

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useContext } from 'react';
 import { QuizContext } from './context/quiz';
 
 import Welcome from './components/Welcome/Welcome';
+import Choose from './components/Choose/Choose';
 import Questions from './components/Questions/Questions';
 import GameOver from './components/GameOver/GameOver';
 
@@ -14,6 +15,7 @@ function App() {
     <div className='App'>
       <h1>Quizzer</h1>
       {quizState.gameStage === "Start" && <Welcome/>}
+      {quizState.gameStage === "Choosing" && <Choose/>}
       {quizState.gameStage === "Playing" && <Questions/>}
       {quizState.gameStage === "End" && <GameOver/>}
     </div>
